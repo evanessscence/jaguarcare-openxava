06b6bffbcfb0d70118f8d88eb9b114b7c87ca4cc
evanessscence <acbellorin@uamv.edu.ni>
2024-05-26 18:39:01 -0600

    Commit corregido

--- src/main/java/com/uam/jaguarcare/modelo/Inventario.java
+++ src/main/java/com/uam/jaguarcare/modelo/Inventario.java
@@ -1,7 +1,6 @@
 package com.uam.jaguarcare.modelo;
 
 import java.time.*;
-<<<<<<< HEAD
 
 import javax.persistence.*;
 
@@ -22,30 +21,4 @@ public class Inventario {
     private Medicamento medicamentoComprado; 
 
  	private Integer cantidadComprada; 
-=======
-import java.util.*;
-
-import javax.persistence.*;
-
-import lombok.*;
-
-@Entity
-@Getter
-@Setter
-public class Compra{
-	@Id
-	private Integer NumeroDeFactura;
-
-	private LocalDate fecha;
-
-@ManyToMany(fetch=FetchType.LAZY)
-	@JoinTable(name = "compra_medicamento", joinColumns = @JoinColumn(name = "compra_id"),
-			inverseJoinColumns = @JoinColumn(name = "medicamento_id"))
-	private List<Medicamento> medicamento;
-	
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name = "proveedor_id")
-	private Proveedor proveedor; 
-
->>>>>>> refs/remotes/origin/master
 }
--- src/main/java/com/uam/jaguarcare/modelo/Visita.java
+++ src/main/java/com/uam/jaguarcare/modelo/Visita.java
@@ -20,29 +20,17 @@ public class Visita {
     private Visitante visitante; 
 
     private LocalDate fecha;
+    
 
     private LocalTime horaDeEntrada;
 
     private LocalTime horaDeSalida;
 
-<<<<<<< HEAD
    @ManyToOne(fetch=FetchType.LAZY, optional = true)
    @DescriptionsList
     Sintomatologia sintomatologia; 
      
-=======
-    @Hidden
-    private LocalTime horadeSalida;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "visitante_id")
-    private Visitante visitante;
-
-   /* @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "recepcionista_id")
-    private Recepcionista AtendidoPor; */
-
->>>>>>> refs/remotes/origin/master
     @Enumerated(EnumType.STRING)
     private Destino destino;
     
